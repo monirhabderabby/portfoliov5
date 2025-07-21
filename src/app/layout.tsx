d8701cef60e91b10060dbb61bbaf39470ae55a3a
Monir Hossain <monirhrabby.personal@gmail.com>
2025-07-21 08:59:03 +0600

    added: meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,29 @@ const lexend = Lexend({
 });
 
 export const metadata: Metadata = {
-  title: "Monir Hossain",
-  description: "Full Stack Web Developer",
+  title: "Monir Hossain | Full Stack Web Developer",
+  description:
+    "Experienced Full Stack Developer specializing in building modern, scalable web applications with Next.js, Prisma, and MongoDB. Focused on clean code, performance, and user experience.",
   keywords:
-    "Full Stack Developer, Web Development, Next.js, React.js, Node.js, Express.js, MongoDB, JavaScript, Frontend Development, Backend Development, software engineer, responsive design, web applications. database management, UI/UX Design, Server-Side Rendering, Client-Side Rendering, Single Page applications.Cross platform development, restful apis, devops, version control, code optimization, TypeScript, Frontend Frameworks , Backend Frameworks, MERN Stack, MEAN Stack,  SEO Best Practices, Open Source Contribution, Web Security, API Integration, Error Handling, Code Versioning, monirrabby",
+    "Monir Hossain, Full Stack Developer, Web Developer, Next.js, React.js, Prisma, MongoDB, Node.js, Express.js, JavaScript, TypeScript, MERN Stack, Frontend Developer, Backend Developer, Software Engineer, Web Applications, REST APIs, UI/UX Design, Server-Side Rendering, Client-Side Rendering, SEO, Web Security, Code Optimization, API Integration, Open Source, DevOps, Git, Responsive Design",
   openGraph: {
+    title: "Monir Hossain | Full Stack Web Developer",
+    description:
+      "Building fast, scalable, and modern web apps using Next.js, Prisma, and MongoDB.",
     images:
       "https://res.cloudinary.com/dzlrpspps/image/upload/v1700748344/Portfolio%20Website/monir_hossain.png",
+    type: "website",
+    url: "https://your-portfolio-domain.com", // replace with your domain
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Monir Hossain | Full Stack Web Developer",
+    description:
+      "Creating modern, performant, and scalable web apps with Next.js, Prisma, and MongoDB.",
+    images: [
+      "https://res.cloudinary.com/dzlrpspps/image/upload/v1700748344/Portfolio%20Website/monir_hossain.png",
+    ],
+    creator: "@yourTwitterHandle", // optional
   },
   publisher: "Monir Hossain",
 };
