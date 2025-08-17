54bb5e686d6d472b6399133f84f1727f8ceb6ea9
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2025-08-17 07:48:25 +0600

    only navbar and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
-import Contact from "@/components/sections/contact";
-import Educations from "@/components/sections/educations";
-import Experiences from "@/components/sections/experiences";
 import Hero from "@/components/sections/hero";
-import Projects from "@/components/sections/projects/projects";
-import Skills from "@/components/sections/skills";
 import { socialData } from "@/data/data";
 import { BASE_URL } from "@/lib/utils";
-import dynamic from "next/dynamic";
 import Head from "next/head";
-const AboutMe = dynamic(() => import("@/components/sections/about-me"), {
-  ssr: false,
-});
-const Achievements = dynamic(
-  () => import("@/components/sections/achivements"),
-  {
-    ssr: false,
-  }
-);
+// const AboutMe = dynamic(() => import("@/components/sections/about-me"), {
+//   ssr: false,
+// });
+// const Achievements = dynamic(
+//   () => import("@/components/sections/achivements"),
+//   {
+//     ssr: false,
+//   }
+// );
 export default function Home() {
   return (
     <>
@@ -38,8 +32,8 @@ export default function Home() {
       <div>
         <div className="px-0  py-8 ">
           <Hero />
-          <AboutMe />
-          <div className="space-y-[100px] md:space-y-[200px] mt-[200px] md:mt-0">
+          {/* <AboutMe /> */}
+          {/* <div className="space-y-[100px] md:space-y-[200px] mt-[200px] md:mt-0">
             <Skills />
             <Projects />
             <div className="py-0 md:py-[80px] bg-black">
@@ -49,10 +43,10 @@ export default function Home() {
 
               <Achievements isSliced />
             </div>
-          </div>
+          </div> */}
         </div>
 
-        <Contact />
+        {/* <Contact /> */}
       </div>
     </>
   );
