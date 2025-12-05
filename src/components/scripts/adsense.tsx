ca2d02081116156d8dbd066654930932852ea1b3
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2025-12-05 21:48:10 +0600

    a

--- src/components/scripts/adsense.tsx
+++ src/components/scripts/adsense.tsx
@@ -15,18 +15,3 @@ const Adsense = ({ pid }: Props) => {
 };
 
 export default Adsense;
-
-export const InfoLinks = () => {
-  return (
-    <>
-      <script type="text/javascript">
-        {" "}
-        var infolinks_pid = 3441994; var infolinks_wsid = 0;{" "}
-      </script>{" "}
-      <script
-        type="text/javascript"
-        src="//resources.infolinks.com/js/infolinks_main.js"
-      ></script>
-    </>
-  );
-};
