ddc8928693c37cad9bc5cc448ab19ac431682d9a
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2025-12-05 21:42:55 +0600

    added: infolinks

--- src/components/scripts/adsense.tsx
+++ src/components/scripts/adsense.tsx
@@ -15,3 +15,18 @@ const Adsense = ({ pid }: Props) => {
 };
 
 export default Adsense;
+
+export const InfoLinks = () => {
+  return (
+    <>
+      <Script type="text/javascript">
+        {" "}
+        var infolinks_pid = 3441994; var infolinks_wsid = 0;{" "}
+      </Script>{" "}
+      <Script
+        type="text/javascript"
+        src="//resources.infolinks.com/js/infolinks_main.js"
+      ></Script>
+    </>
+  );
+};
